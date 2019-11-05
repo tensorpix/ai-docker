f6dc5066e5207f3722b035353da1db08ce433873
Mladen Fernežir <mfernezir@gmail.com>
2019-11-05 10:54:29 +0100

    setuser.sh improvements, pip from requirements file

--- setuser.sh
+++ setuser.sh
@@ -6,7 +6,8 @@ set -u
 # Setup user if this is the first run
 CONTAINER_ALREADY_STARTED="/etc/container_already_started"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
-    adduser --uid $ID --disabled-password --gecos '' $NAME
+    groupadd --gid $GID $NAME
+    adduser --uid $ID --gid $GID --disabled-password --gecos '' $NAME
     adduser $NAME sudo
     echo "$NAME ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$NAME
     chmod 0440 /etc/sudoers.d/$NAME
@@ -22,33 +23,21 @@ if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
         echo "source /usr/share/bash-completion/completions/git" >> /home/$NAME/$SHELL_RC_FILE
     fi
 
-
-    # if $ID not 1000, create user "datascience" with id 1000
-    # useful for train machines
-    if (($ID != 1000)); then
-        DATASCIENCE_USER="datascience"
-        adduser --uid 1000 --disabled-password --gecos '' $DATASCIENCE_USER
-        adduser $DATASCIENCE_USER sudo
-        echo "$DATASCIENCE_USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$DATASCIENCE_USER
-        chmod 0440 /etc/sudoers.d/$DATASCIENCE_USER
-        # add $ID user to datascience group, and vice versa
-        adduser $NAME $DATASCIENCE_USER
-        adduser $DATASCIENCE_USER $NAME
+    # optionally set a common datascience group
+    if [[ -v DS_ID ]]; then
+        groupadd --gid $DS_ID "datascience"
+        adduser $NAME "datascience"
     fi
 
-    # setup code path if DS_CODE_PATH set
-    if [[ -v DS_CODE_PATH ]]; then
-        echo "DS_CODE_PATH=$DS_CODE_PATH" >> /home/$NAME/$SHELL_RC_FILE
-        path=\$DS_CODE_PATH/computer_vision:
-        path+=\$DS_CODE_PATH/ds-common:
-        path+=\$DS_CODE_PATH/intermediary:
-        path+=\$DS_CODE_PATH/newsletter_personalization:
-        path+=\$DS_CODE_PATH/text_analysis:
-        path+=\$DS_CODE_PATH/tree_manager
+    # setup code path if CODE_PATH set
+    if [[ -v CODE_PATH ]]; then
+        echo "CODE_PATH=$CODE_PATH" >> /home/$NAME/$SHELL_RC_FILE
+        path=\$CODE_PATH/research/vision:
+        path+=\$CODE_PATH/external-projects
         echo "export PYTHONPATH=$path" >> /home/$NAME/$SHELL_RC_FILE
     fi
 
-    # note first container run
+    # mark first container run
     touch $CONTAINER_ALREADY_STARTED
 fi
 
